17f414e3e74eb47490ab80d9ed33e4640f046699
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-08-14 11:55:30 +0100

    initial commit of boiler plate codes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.get("/", (req, res)=>{
+ res.send("<h1>Hello from Node</h1>")
+})
 
 app.listen(PORT, ()=>{
     console.log(`server listening at ${PORT}`)
